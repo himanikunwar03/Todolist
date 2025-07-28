b09ebdb90d885546a53a2a630aa1b80c874afc99
himanikunwar098@gmail.com <himanikunwar098@gmail.com>
2025-07-28 22:09:25 +0545

    login

--- app/src/main/java/com/example/todolist/view/LoginActivity.kt
+++ app/src/main/java/com/example/todolist/view/LoginActivity.kt
@@ -7,16 +7,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
+//import androidx.compose.material.icons.filled.baseline_visibility_24.xml
+//import androidx.compose.material.icons.filled.baseline_visibility_off_24.xml
+
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.todolist.R
 import com.example.todolist.ui.theme.TodolistTheme // change to your actual theme
 
 class LoginActivity : ComponentActivity() {
@@ -72,11 +76,21 @@ fun LoginScreen() {
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
             visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
             trailingIcon = {
-                val image = if (passwordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff
-                val desc = if (passwordVisible) "Hide password" else "Show password"
-                IconButton(onClick = { passwordVisible = !passwordVisible }) {
-                    Icon(imageVector = image, contentDescription = desc)
+                val image = if (passwordVisible)
+                    IconButton(onClick = {}) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_visibility_24),
+                            contentDescription = "Show password"
+                        )
+                    }else{
+                        painterResource(R.drawable.baseline_visibility_off_24)
+                    }
+                val desc= if (passwordVisible) "Hide password" else "Show password"
+                IconButton(onClick = {passwordVisible = !passwordVisible}) {
+                    Icon(painter = painter, contentDescription = desc)
                 }
+
+
             },
             modifier = Modifier.fillMaxWidth()
         )
